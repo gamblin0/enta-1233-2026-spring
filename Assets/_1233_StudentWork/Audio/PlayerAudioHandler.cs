f26b852b6e1e17c5bcb7c56924daa01a710d251f
gamblin0 <baraybolat@gmail.com>
2026-02-01 21:20:45 -0800

    setting up sound clips for animations

--- Assets/_1233_StudentWork/Audio/PlayerAudioHandler.cs
+++ Assets/_1233_StudentWork/Audio/PlayerAudioHandler.cs
@@ -4,13 +4,22 @@ public class PlayerAudioHandler : MonoBehaviour
 {
 
     [SerializeField] private AudioSource _footstepSource;
-
+    [SerializeField] private AudioSource _landingSource;
+    [SerializeField] private AudioSource _jumpingSource;
 
     public void PlayFootstep()
     {
         _footstepSource?.Play();
     }
 
+    public void PlayLanding()
+    {
+        _landingSource?.Play();
+    }
 
-   
+    public void PlayJumping()
+    {
+        _jumpingSource?.Play();
+    }
+
 }
