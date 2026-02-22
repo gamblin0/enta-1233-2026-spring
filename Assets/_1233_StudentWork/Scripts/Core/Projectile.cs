d2e87f4251ddd0cb15cfbafe312d0022ef1b07c4
gamblin0 <baraybolat@gmail.com>
2026-02-21 16:43:36 -0800

    ProjectileWeapon scripts

--- Assets/_1233_StudentWork/Scripts/Core/Projectile.cs
+++ Assets/_1233_StudentWork/Scripts/Core/Projectile.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 [RequireComponent (typeof(Rigidbody))]
@@ -15,6 +16,49 @@ public class Projectile : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _rb.useGravity = _useGravity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //dont hit source
+        if (collision.gameObject == _source) return;
+
+        //check if we hit something damageable
+        var damageReciever = collision.gameObject.GetComponent<IDamageReciever>();
+        if (damageReciever != null)
+        {
+            var info = new DamageInfo
+            {
+                Amount = _damage,
+                Source = _source,
+                HitPoint = collision.contacts[0].point,
+                HitNormal = collision.contacts[0].normal,
+            };
+            damageReciever.ApplyDamage(info);
+        }
+
+        //destroy on impact
+        Destroy(gameObject);
+    }
+
+    public void Launch(Vector3 direction, GameObject source)
+    {
+        _source = source;
+        _rb.linearVelocity = direction.normalized * _speed;
+        transform.forward = direction;
+        Destroy(gameObject, _lifetime); //simple destruction for now
+    }
+
+    public void LaunchWithVelocity(Vector3 velocity, GameObject source)
+    {
+        _source = source;
+        _rb.linearVelocity = velocity;
+        if (velocity.sqrMagnitude > 0.001f)        
+            transform.forward = velocity;
+        _rb.useGravity = true; //force gravity for arc shots
+        Destroy(gameObject, _lifetime);
+
         
     }
 }
